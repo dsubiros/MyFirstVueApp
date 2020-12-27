fc5e8b4f891ca6b89be0c23c42b72bb305f4b3b5
Dariel Subiros <dsubiros@gmail.com>
2020-12-27 07:54:49 -0500

    Differentiate db user between Linux and Windows. It must be refactored after adding dacpac generation stuff.

--- MyFirstVueApp.DataModel/Models/BloggingContext.cs
+++ MyFirstVueApp.DataModel/Models/BloggingContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -26,7 +27,8 @@ namespace MyFirstVueApp.Database.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.;Initial Catalog=Blogging;User Id=tareko;Password=A123456*;MultipleActiveResultSets=true");
+                optionsBuilder.UseSqlServer($"Server=.;Initial Catalog=Blogging;User Id={(RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "tareko" : "sa")};Password=A123456*;MultipleActiveResultSets=true");
+                // optionsBuilder.UseSqlServer("Server=.;Initial Catalog=Blogging;User Id=tareko;Password=A123456*;MultipleActiveResultSets=true");
             }
         }
 
